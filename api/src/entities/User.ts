c89a51ae7ba7aef3e8ef0b3280b13cebc2a2155d
Ben Awad <benawad97@gmail.com>
2020-12-22 09:49:39 -0600

    8_typeorm

---
+++ api/src/entities/User.ts
@@ -0,0 +1,10 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("text")
+  name: string;
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,22 @@
+import "reflect-metadata";
 import express from "express";
+import { createConnection } from "typeorm";
+import { __prod__ } from "./constants";
+import { join } from "path";
+import { User } from "./entities/User";
 
 const main = async () => {
+  await createConnection({
+    type: "postgres",
+    database: "vstodo",
+    entities: [join(__dirname, "./entities/*.*")],
+    logging: !__prod__,
+    synchronize: !__prod__,
+  });
+
+  const user = await User.create({ name: "bob" }).save();
+
+  console.log({ user });
   const app = express();
   app.get("/", (_req, res) => {
     res.send("hello");
